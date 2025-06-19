0496fe3dc677fddc8f11df173616bda48d5cf667
thais <thais-pacheco@educar.rs.gov.br>
2025-06-19 20:50:39 -0300

    enviando dados do frontend com react

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -1,8 +1,100 @@
-import React from "react";
+import React from 'react'
+import {Box, Button, Typography} from '@mui/material'
+import MyDatePickerField from './forms/MyDatePickerField'
+import MyTextField from './forms/MyTextField'
+import MySelectField from './forms/MySelectField'
+import MyMultilineField from './forms/MyMultilineField'
+import {useForm} from 'react-hook-form'
+import AxiosInstance from './Axios'
+import Dayjs  from 'dayjs'
+import {useNavigate} from 'react-router-dom'
 
 const Create = () => {
+
+    const navigate = useNavigate()
+    const defaultValues = {
+        name : '',
+        comments: '',
+        status: '',
+        start_date: '',
+        end_date: '',
+    }
+    const {handleSubmit, reset, setValue, control} = useForm({defaultValues: defaultValues})
+    const submission = (data) => {
+        const StartDate = Dayjs(data.start_date["%d"]).format("YYYY-MM-DD")
+        const EndDate = Dayjs(data.end_date["%d"]).format("YYYY-MM-DD")
+
+        AxiosInstance.post(
+            `project/`,{
+                name: data.name,
+                status: data.status,
+                comments: data.comments,
+                start_date: StartDate,
+                end_date: EndDate,
+            }
+        )
+        .then((res) =>{
+            navigate(`/`)
+        })
+
+    }
     return (
-        <div>create</div>
+        <div>
+            <form onSubmit={handleSubmit(submission)}>
+                <Box sx={{display:'flex', width:'100%', backgroundColor:'#00003f', marginBottom:'10px'}}>
+                <Typography sx={{marginLeft:'20px', color:'#fff'}}>
+                    Create records
+                </Typography>
+                </Box>
+
+            <Box sx={{display:'flex', width:'100%', boxShadow:3, padding:4, flexDirection:'column'}}>
+
+            <Box sx={{display:'flex', justifyContent:'space-around', marginBottom:'40px'}}>
+                <MyTextField
+                        label="Name"
+                        name="name"
+                        control={control}
+                        placeholder="provide a project name"
+                        width={'30%'}
+                    />
+                    <MyDatePickerField
+                    label="Start date"
+                    name="start_date"
+                    control={control}
+                    width={'30%'}
+                    />
+                    <MyDatePickerField
+                    label="End date"
+                    name="end_date"
+                    control={control}
+                    width={'30%'}
+                    />
+                </Box>
+
+                <Box sx={{display:'flex', justifyContent:'space-around'}}>
+                    <MyMultilineField
+                        label="Comments"
+                        name="comments"
+                        control={control}
+                        placeholder="provide project comments"
+                        width={'30%'}
+                    />
+                    <MySelectField
+                    label="Status"
+                    name="status"
+                    control={control}
+                    width={'30%'}
+                    />
+                    <Box sx={{width:'30%'}}>
+                    <Button variant="contained" type="submit" sx={{width:'100%'}}>
+                        Submit
+                    </Button>
+                    </Box>
+                </Box>
+
+            </Box>
+            </form>
+        </div>
     )
 }
 
---
+++ frontend/src/components/forms/MySelectField.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import {Controller} from 'react-hook-form'
+
+export default function MySelectField(props) {
+  const [age, setAge] = React.useState('');
+  const{label, name, control, width} = props
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+        <FormControl variant="standard" sx={{width:{width}}}>
+        <InputLabel id="demo-simple-select-filled-label">{label}</InputLabel>
+         <Controller
+            name =  {name}
+            control={control}
+            render={({
+                field:{onChange, value},
+                fieldState:{error},
+                formState,
+            }) => (
+                <Select
+                    labelId="demo-simple-select-filled-label"
+                    id="demo-simple-select-filled"
+                    onChange={onChange}
+                    value={value}
+                    >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={"Aberto"}>Aberta</MenuItem>
+                    <MenuItem value={"Em andamento"}>Em andamento</MenuItem>
+                    <MenuItem value={"Concluída"}>Conluída</MenuItem>
+                </Select>
+            )
+        }
+          />
+      </FormControl>
+  
+  );
+}
